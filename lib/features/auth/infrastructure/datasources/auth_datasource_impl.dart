dd2b295ceb6f0ed0029073452984014e1340df96
AngelTravieso <angeltraviesoc@gmail.com>
2023-11-07 22:10:23 -0400

    talker_dio_logger agregado

--- lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:dio/dio.dart';
+import 'package:talker_dio_logger/talker_dio_logger.dart';
 
 import 'package:teslo_app/config/config.dart';
 import 'package:teslo_app/features/auth/domain/domain.dart';
@@ -9,7 +12,17 @@ class AuthDataSourceImpl extends AuthDataSource {
     BaseOptions(
       baseUrl: Environment.apiUrl,
     ),
-  );
+  )..interceptors.add(
+      TalkerDioLogger(
+        settings: const TalkerDioLoggerSettings(
+          printRequestHeaders: true,
+          printResponseHeaders: true,
+          printResponseMessage: true,
+          printRequestData: true,
+          printResponseData: true,
+        ),
+      ),
+    );
 
   @override
   Future<User> checkAuthStatus(String token) {
