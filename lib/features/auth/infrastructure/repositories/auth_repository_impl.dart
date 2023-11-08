5dfce2388f980eeae5a4d5284f86f609f3e76749
AngelTravieso <angeltraviesoc@gmail.com>
2023-11-07 20:08:51 -0400

    Implementacion del AuthDataSource - Login

--- lib/features/auth/infrastructure/repositories/auth_repository_impl.dart
+++ lib/features/auth/infrastructure/repositories/auth_repository_impl.dart
@@ -1,7 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:teslo_app/config/config.dart';
 import 'package:teslo_app/features/auth/domain/domain.dart';
 import 'package:teslo_app/features/auth/infrastructure/infrastructure.dart';
 
 class AuthRepositoryImpl extends AuthRepository {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: Environment.apiUrl,
+    ),
+  );
+
   final AuthDataSource dataSource;
 
   AuthRepositoryImpl(AuthDataSource? dataSource)
@@ -13,8 +21,19 @@ class AuthRepositoryImpl extends AuthRepository {
   }
 
   @override
-  Future<User> login(String email, String password) {
-    return dataSource.login(email, password);
+  Future<User> login(String email, String password) async {
+    try {
+      final response = await dio.post('/login', data: {
+        'email': email,
+        'password': password,
+      });
+
+      final user = UserMapper.userJsonToEntity(response.data);
+
+      return user;
+    } catch (e) {
+      throw UnimplementedError();
+    }
   }
 
   @override
